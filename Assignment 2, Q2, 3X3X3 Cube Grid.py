de70520ddc001299225b3d4bef9c0c36de86b480
assassin-adi <adityakrishna7000@gmail.com>
2025-05-23 22:03:41 +0530

    Create Assignment 2, Q2, 3X3X3 Cube Grid.py

---
+++ Assignment 2, Q2, 3X3X3 Cube Grid.py
@@ -0,0 +1,23 @@
+from vpython import *
+
+s = 1
+f = 0.95
+d = 0.02
+
+for a in [1, 0, -1]:
+    for b in [1, 0, -1]:
+        for c in [1, 0, -1]:
+            pos = vector(a, b, c)
+            box(pos=pos, size=vector(s, s, s), color=vector(0.1, 0.1, 0.1))
+            if c == 1:
+                box(pos=pos + vector(0, 0, s/2 + d/2), size=vector(f, f, d), color=color.white)
+            if c == -1:
+                box(pos=pos + vector(0, 0, -s/2 - d/2), size=vector(f, f, d), color=color.yellow)
+            if a == -1:
+                box(pos=pos + vector(-s/2 - d/2, 0, 0), size=vector(d, f, f), color=color.blue)
+            if a == 1:
+                box(pos=pos + vector(s/2 + d/2, 0, 0), size=vector(d, f, f), color=color.green)
+            if b == 1:
+                box(pos=pos + vector(0, s/2 + d/2, 0), size=vector(f, d, f), color=color.red)
+            if b == -1:
+                box(pos=pos + vector(0, -s/2 - d/2, 0), size=vector(f, d, f), color=color.orange)
