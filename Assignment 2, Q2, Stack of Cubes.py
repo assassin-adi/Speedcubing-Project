a34ba0a41922985ebe2e79a0431b106b6282c11c
assassin-adi <adityakrishna7000@gmail.com>
2025-05-23 22:39:15 +0530

    Create Assignment 2, Q2, Stack of Cubes.py

---
+++ Assignment 2, Q2, Stack of Cubes.py
@@ -0,0 +1,16 @@
+from vpython import *
+
+box(pos=vector(0,0,0), size=vector(1,1,1), color=color.red)
+label(pos=vector(0,0,0), text="red", height=10, color=color.red)
+
+box(pos=vector(0,1,0), size=vector(1,1,1), color=color.orange)
+label(pos=vector(0,1,0), text="orange", height=10, color=color.orange)
+
+box(pos=vector(0,2,0), size=vector(1,1,1), color=color.blue)
+label(pos=vector(0,2,0), text="blue", height=10, color=color.blue)
+
+box(pos=vector(0,3,0), size=vector(1,1,1), color=color.yellow)
+label(pos=vector(0,3,0), text="yellow", height=10, color=color.yellow)
+
+box(pos=vector(0,4,0), size=vector(1,1,1), color=color.white)
+label(pos=vector(0,4,0), text="white", height=10, color=color.white)
